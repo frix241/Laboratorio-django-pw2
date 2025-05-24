c5084d6da7ceb224c04f834d880c81c6814d6f74
frix241 <ariverosv@unsa.edu.pe>
2025-05-24 18:06:48 -0500

    Creando la vista para agregar alumnos con el formulario AlumnoForm

--- src/sistema_notas/gestor_notas/views.py
+++ src/sistema_notas/gestor_notas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import AlumnoForm, CursoForm, NotaAlumnoPorCursoForm
+#  Create your views here.case
 
-# Create your views here.
+
+def agregar_alumno(request):
+    if request.method == "POST":
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("agregar_alumno")
+    else:
+        form = AlumnoForm()
+    return render(request, "gestor_notas/agregar_alumno.html", {"form": form})
