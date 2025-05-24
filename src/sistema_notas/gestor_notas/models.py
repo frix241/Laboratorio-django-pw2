d2b4cbee70a5170c816f4c504a462991b8a54a01
frix241 <ariverosv@unsa.edu.pe>
2025-05-24 17:45:49 -0500

    Añadiendo el campo de cui y apellido para la clase Alumno y añadiendo el apeelido al metodo string

--- src/sistema_notas/gestor_notas/models.py
+++ src/sistema_notas/gestor_notas/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Alumno(models.Model):
     nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    cui = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} {self.apellido}"
