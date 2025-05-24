1c2f8a1e894be6a39a6b14b864107e71283c7260
frix241 <ariverosv@unsa.edu.pe>
2025-05-24 18:53:57 -0500

    colocando el form de alumno como predeterminado para la ruta gestor_notas

--- src/sistema_notas/gestor_notas/urls.py
+++ src/sistema_notas/gestor_notas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("alumno/", views.agregar_alumno, name="agregar_alumno"),
+    path("", views.agregar_alumno, name="agregar_alumno"),
     path("curso/", views.agregar_curso, name="agregar_curso"),
     path("nota/", views.agregar_nota, name="agregar_nota"),
 ]
