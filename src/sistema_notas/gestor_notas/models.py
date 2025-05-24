49a91d26fedf06504f09b1e80d4839165c1fba08
frix241 <ariverosv@unsa.edu.pe>
2025-05-24 17:53:22 -0500

    Creando el modelo NotaAlumnoPorCurso con 3 campos, uno numerico y dos de clave foranea

--- src/sistema_notas/gestor_notas/models.py
+++ src/sistema_notas/gestor_notas/models.py
@@ -17,3 +17,11 @@ class Curso(models.Model):
 
     def __str__(self):
         return f"{self.nombre}"
+
+class NotaAlumnoPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}: {self.nota}"
