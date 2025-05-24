2b36be07e2c6f0f8cd7ee8f610284353aed001b1
frix241 <ariverosv@unsa.edu.pe>
2025-05-24 18:31:51 -0500

    Añadiendo la aplicacion al proyecto y corrigiendo un error tipografico

--- src/sistema_notas/gestor_notas/urls.py
+++ src/sistema_notas/gestor_notas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
+urlpatterns = [
     path("alumno/", views.agregar_alumno, name="agregar_alumno"),
     path("curso/", views.agregar_curso, name="agregar_curso"),
     path("nota/", views.agregar_nota, name="agregar_nota"),
