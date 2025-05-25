de96eb6b63605a4e6aff398c78c27ceb1ffb04b0
Rodrigo EstPal <restefanerop@unsa.edu.pe>
2025-05-24 21:17:40 -0500

    (new app) parte 3.- Se creo los modelos alumno curso y notaAlumnoCurso

--- new-app/sistema_notas/gestion/models.py
+++ new-app/sistema_notas/gestion/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class NotaAlumnoCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
+
+    class Meta:
+        unique_together = ('alumno', 'curso')  # Para evitar duplicados
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}: {self.nota}"
